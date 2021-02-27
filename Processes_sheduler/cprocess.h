c3deb9a30742cc3967f1bee12975a54756f4e1dc
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-02-27 01:19:51 +0100

    Expansion cProcess.h
    Add fields in cProcess class.

--- Processes_sheduler/cprocess.h
+++ Processes_sheduler/cprocess.h
@@ -14,6 +14,39 @@ using namespace std;
  */
 class cProcess
 {
+    /*
+     * vTimeReadiness - czas uplywajacy od momentu rozpoczecie
+     * przetwarzania procesow do momentu gotowsci procesu
+     * do wykonywania.
+     */
+    typeTime vTimeReadiness;
+
+    /*
+     * vTimeWaiting - czas oczekiwania na wykonanie procesu.
+     */
+    typeTime vTimeWaiting;
+
+    /*
+     * vTimeDoing - czas wykonywania procesu.
+     */
+    typeTime vTimeDoing;
+
+    /*
+     * vTimeDone - czas procesu jaki zostal juz wykonany.
+     */
+    typeTime vTimeDone;
+
+    /*
+     * vTimeToDo - czas jaki pozostal do calkowitego wykonania
+     * procesu.
+     */
+    typeTime vTimeToDo;
+
+    /*
+     * vTimeProcessing - calkowity czas przetwarzania procesu
+     * (oczekiwanie + wykonywanie).
+     */
+    typeTime vTimeProcessing;
 public:
     /*
      * cProcess() - konstruktor wywolywany bez argumentow.
