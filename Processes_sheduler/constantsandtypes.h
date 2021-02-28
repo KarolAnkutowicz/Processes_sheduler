d956651244eb1f51b173fcffdfa3ffab5f60e936
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-02-28 15:24:50 +0100

    Add constants definitions
    Add definitions of types ansd constants using in algorithms.

--- Processes_sheduler/constantsandtypes.h
+++ Processes_sheduler/constantsandtypes.h
@@ -8,8 +8,34 @@ using namespace std;
 
 /*
  * unsigned int typeTime - zdefiniowany synonim typu
+ * dla czasu trwania procesu.
  */
 typedef unsigned int typeTime;
+
+/*
+ * unsigned short typeNumberProcess - zdefiniowany synonim typu
+ * dla numeracji procesow.
+ */
+typedef unsigned short typeNumberProcess;
+
+
+
+/*
+ * typeTime constMaxTime - zdefiniowany maksymalny czas gotowosci
+ * oraz maksymalmny czas trwania procesu.
+ */
+const typeTime constMaxTime = 20;
+
+/*
+ * typeNumberProcess constProcesses - zdefiniowana liczba procesow
+ * w ciagu.
+ */
+const typeNumberProcess constProcesses = 10;
+
+/*
+ * typeNumberProcess constSeries - zdefiniowana liczba ciagow procesow.
+ */
+const typeNumberProcess constSeries = 10;
 
 #endif // CONSTANTSANDTYPES_H
 
