a51ed4b060af0e0de2593539a4ec22bb9c7f20f2
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-03-10 13:09:23 +0100

    Methods in cShedulingAgorithms
    Beginning write methods in cShedulingAlgorithms class (cshedulingalgorithms.cpp).

--- Processes_sheduler/cshedulingalgorithms.cpp
+++ Processes_sheduler/cshedulingalgorithms.cpp
@@ -50,5 +50,138 @@ void cShedulingAlgorithms::mMakeSRT()
 }
 
 
+
+/*
+ * mDrawProcesses()
+ */
+void cShedulingAlgorithms::mDrawProcesses()
+{
+}
+
+/*
+ * mResetAllProcesses()
+ */
+void cShedulingAlgorithms::mResetAllProcesses()
+{
+}
+
+/*
+ * mReadProcessesFromFile()
+ */
+void cShedulingAlgorithms::mReadProcessesFromFile()
+{
+}
+
+/*
+ * mWriteProcessesToFile()
+ */
+void cShedulingAlgorithms::mWriteProcessesToFile()
+{
+}
+
+
+
+/*
+ * mSortingAscendingProcesses(typeNumberProcess aSeries)
+ */
+void cShedulingAlgorithms::mSortingAscendingProcesses(typeNumberProcess aSeries)
+{
+}
+
+/*
+ * mSortingAscendingAllProcesses()
+ */
+void cShedulingAlgorithms::mSortingAscendingAllProcesses()
+{
+}
+
+/*
+ * mSortingDescendingProcesses(typeNumberProcess aSeries)
+ */
+void cShedulingAlgorithms::mSortingDescendingProcesses(typeNumberProcess aSeries)
+{
+}
+
+/*
+ * mSortingDescendingAllProcesses()
+ */
+void cShedulingAlgorithms::mSortingDescendingAllProcesses()
+{
+}
+
+
+
+/*
+ * mResetAllTotalTimeDoing()
+ */
+void cShedulingAlgorithms::mResetAllTotalTimeDoing()
+{
+}
+
+/*
+ * mCalculateTotalTimeDoing(typeNumberProcess aSeries)
+ */
+void cShedulingAlgorithms::mCalculateTotalTimeDoing(typeNumberProcess aSeries)
+{
+}
+
+
+
+/*
+ * mResetAllAverageTimeDoing()
+ */
+void cShedulingAlgorithms::mResetAllAverageTimeDoing()
+{
+}
+
+/*
+ * mCalculateAverageTimeDoing(typeNumberProcess aSeries)
+ */
+void cShedulingAlgorithms::mCalculateAverageTimeDoing(typeNumberProcess aSeries)
+{
+}
+
+
+
+/*
+ * mResetAllTotalTimeProcessing()
+ */
+void cShedulingAlgorithms::mResetAllTotalTimeProcessing()
+{
+}
+
+/*
+ * mCalculateTotalTimeProcessing(typeNumberProcess aSeries)
+ */
+void cShedulingAlgorithms::mCalculateTotalTimeProcessing(typeNumberProcess aSeries)
+{
+}
+
+
+
+/*
+ * mResetAllAverageTimeProcessing()
+ */
+void cShedulingAlgorithms::mResetAllAverageTimeProcessing()
+{
+}
+
+/*
+ * mCalculateAverageTimeProcessing(typeNumberProcess aSeries)
+ */
+void cShedulingAlgorithms::mCalculateAverageTimeProcessing(typeNumberProcess aSeries)
+{
+}
+
+
+
+/*
+ * mWriteResultsToFile()
+ */
+void cShedulingAlgorithms::mWriteResultsToFile()
+{
+}
+
+
 /* cshedulingalgorithms.cpp */
 /********** END_OF_FILE **********/
